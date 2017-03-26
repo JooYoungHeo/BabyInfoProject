01e6bdade4b14d493b1db8c82dfcff916591e40a
JooYoungHeo <jyhur900226@naver.com>
2017-03-26 20:08:37 +0900

    [작업] input page 작성

---
+++ MainPage.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center'
+  },
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565'
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch'
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    marginTop: 5,
+    flex: 4,
+    borderWidth: 1,
+    borderColor: '#48bbec',
+    borderRadius: 8,
+    color: '#48bbec'
+  },
+  button: {
+    height: 36,
+    flex: 1,
+    padding: 4,
+    marginRight: 5,
+    marginTop: 5,
+    flexDirection: 'row',
+    backgroundColor: '#48bbec',
+    borderColor: '#48bbec',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+  image: {
+    width: 240,
+    height: 160
+  }
+});
+
+class MainPage extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      location: '',
+      age: ''
+    }
+  }
+
+  locationChange(e){
+    this.setState({
+      location: e.nativeEvent.text
+    });
+  }
+
+  ageChange(e){
+    this.setState({
+      age: e.nativeEvent.text
+    });
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Nursery Information!
+        </Text>
+        <Text style={styles.description}>
+          input your location & baby age
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            value={this.state.location}
+            onChange={this.locationChange.bind(this)}
+            placeholder='input your location' />
+        </View>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            value={this.state.age}
+            onChange={this.ageChange.bind(this)}
+            placeholder='input your baby age' />
+        </View>
+        <View style={styles.flowRight}>
+          <TouchableHighlight style={styles.button} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Go</Text>
+          </TouchableHighlight>
+        </View>
+        <Image source={{uri: 'https://dsh602wr9lxr7.cloudfront.net/suzy.jpg'}} style={styles.image} />
+      </View>
+    )
+  }
+}
+
+export default MainPage;
--- index.ios.js
+++ index.ios.js
@@ -8,46 +8,23 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View
+  NavigatorIOS
 } from 'react-native';
+
+import MainPage from './MainPage';
 
 export default class BabyInfoProject extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS style={styles.container} initialRoute={{title: 'Nursery', component: MainPage}} />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('BabyInfoProject', () => BabyInfoProject);
